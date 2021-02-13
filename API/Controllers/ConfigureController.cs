0e8df20bbf5938de05233d21ff58ce529a2d95ca
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-13 01:15:38 -0300

    modelo pronto agora é somente replicar

---
+++ API/Controllers/ConfigureController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.ViewModel;
+using Infrastructure.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        [AllowAnonymous]
+        public Welcome Index()
+        {
+            return new Welcome();
+        }
+
+        [HttpGet]
+        [Route("/config/start")]
+        [AllowAnonymous]
+        public async Task<string> InicialConfig()
+        {
+            var sql = new SqlDriver();
+            //await sql.CreateTable<Person>();
+            //await sql.CreateTable<Address>();
+            //await sql.CreateTable<Model>();
+            //await sql.CreateTable<Brand>();
+
+            return "Sistema configurado";
+        }
+    }
+}
